029fad96cbe4455feefe5e6ca2f5c0b33664a7e3
Kevin <mrhallonline@gmail.com>
2019-10-11 21:58:24 -0500

    completed 3rd and 4th

---
+++ 03ListLessThanTen.py
@@ -0,0 +1,8 @@
+# Take a list, say for example this one:
+#   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# and write a program that prints out all the elements of the list that are less than 5.
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for i in a:
+    if (i<5):
+        print(i)
--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 Working through practice exercises from [PracticePython.Org](https://www.practicepython.org/)
 1: Character Input
 2: Odd Or Even
+3: List Less Than Ten  
+4: Divisors  
  
