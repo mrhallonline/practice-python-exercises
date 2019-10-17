36cb2e53ee0425ccf7685fe1b1bc14c0ee288dde
Kevin <mrhallonline@gmail.com>
2019-10-17 10:53:52 -0500

    completed rps script

--- 07rockpaperscissors.py
+++ 07rockpaperscissors.py
@@ -5,5 +5,30 @@
 # Scissors beats paper
 # Paper beats rock
 
-playerSelection = input("enter rock, paper, or scissors!");
-rps = [rock, paper, scissors];
+import random
+
+rps = ["rock", "paper", "scissors"]
+playerSelection = input("Enter rock, paper, or scissors! ")
+computerSelection = random.choice(rps)
+
+def round(player, computer):
+    winner= "You win " + player + " beats " + computer
+    loser=  "You lose " + computer + " beats " + player
+    draw= "It's a draw " + player + " ties " + computer 
+    if player==computer:
+        return draw
+    elif player == "rock" and computer == "scissors":
+        return winner
+    elif player == "rock" and computer == "paper":
+        return loser
+    elif player=="paper" and computer == "rock":
+        return winner
+    elif player=="paper" and computer == "scissors":
+        return loser
+    elif player=="scissors" and computer=="paper":
+        return winner
+    elif player=="scissors" and computer=="rock":
+        return loser
+
+print (round(playerSelection, computerSelection))
+
