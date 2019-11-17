71c72c8ca1a9b76c5f2a10c6b495b57975b4c6c4
Kevin <mrhallonline@gmail.com>
2019-11-17 02:13:06 -0500

    update practice

--- practicePage.py
+++ practicePage.py
@@ -200,3 +200,44 @@
 #     print(tripleWord)
 
 # paper_doll('Hello')
+
+# BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. If their sum exceeds 21 and there's an eleven, reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'
+# blackjack(5,6,7) --> 18
+# blackjack(9,9,9) --> 'BUST'
+# blackjack(9,9,11) --> 19
+
+# def blackjack(a,b,c):
+#     roundOne = a+b+c
+#     if (roundOne<=21):
+#         return roundOne
+#     elif (roundOne > 21 and 11 in blackjack):
+#             roundTwo = roundOne-10
+#             if (roundTwo > 21):
+#                 return "Bust"
+#             else:
+#                 return roundTwo
+#     else:
+#         return "Bust" 
+    
+# def blackjack(a,b,c):
+#     thisTup = (a,b,c)
+#     roundOne = a+b+c
+#     x=thisTup.count(11)
+#     if (roundOne<=21):
+#         return (roundOne)
+#     elif (roundOne > 21 and x > 0):
+#             roundTwo = roundOne-10
+#             if (roundTwo > 21):
+#                 return ("Bust")
+#             else:
+#                 return (roundTwo)
+#     else:
+#         return ("Bust")
+
+SUMMER OF '69: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers.
+summer_69([1, 3, 5]) --> 9
+summer_69([4, 5, 6, 7, 8, 9]) --> 9
+summer_69([2, 1, 6, 9, 11]) --> 14
+
+def summer_69(arr):
+    pass
