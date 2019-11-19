e00e13ee0804cf9c739289513a37b9cca7bdd197
Kevin <mrhallonline@gmail.com>
2019-11-18 20:49:03 -0500

    fixed fib practice to include 0 and 1, also adjusted range

--- 13fibonacci.py
+++ 13fibonacci.py
@@ -1,14 +1,18 @@
 # Write a program that asks the user how many Fibonacci numbers to generate and then generates them. Take this opportunity to think about how you can use functions. Make sure to ask the user to enter the number of numbers in the sequence to generate.(Hint: The Fibonacci sequence is a sequence of numbers where the next number in the sequence is the sum of the previous two numbers in the sequence. The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, …)
 
+        # fibSum = sum(fibList)
 def fibGenerator(fibNumber):
-    fibList = [1]
-    for i in range (fibNumber):
-        fibSum = sum(fibList)
-        if (fibSum <= 1):
-            fibList.append(1)
-        else:
-            fibList.append(fibList[-1]+fibList[-2])
-    print(fibList)
+        if (fibNumber == 0):
+            fibList = []
+        elif (fibNumber == 1):
+            fibList = [1]
+        elif (fibNumber == 2): 
+            fibList = [1,1]
+        elif (fibNumber>=3):
+            fibList = [1,1]    
+            for i in range (fibNumber-2):
+                fibList.append(fibList[-1]+fibList[-2])
+        print(fibList)
 
 fibNumber = int(input("How many Fibonacci numbers should I generate?: "))
 fibGenerator(fibNumber)
