69a7c822d64a96d9464e41b371f2f4716623fae6
Kevin <mrhallonline@gmail.com>
2019-11-21 23:57:36 -0500

    cow and bulls challenge

--- practicePythonExercises/17decodeAWebpage.py
+++ practicePythonExercises/17decodeAWebpage.py
@@ -4,6 +4,23 @@ import requests
 
 url = 'http://jamaica-gleaner.com/'
 source = requests.get(url).text
-soup = BeautifulSoup(source, "html5lib")
+soup = BeautifulSoup(source, "lxml")
 
-print(soup.prettify())
+
+# print(titles)
+
+for h2 in soup.find_all('h2'):
+    try:
+        titles = h2.a.text
+    except:
+         titles = 'no title found'
+    print(titles)
+    print('-------')
+
+for h3 in soup.find_all('h3'):
+    try:
+        sub_titles = h3.a.text
+    except:
+        sub_titles = "no subtitles found"
+    print(sub_titles)
+    print('-------')
