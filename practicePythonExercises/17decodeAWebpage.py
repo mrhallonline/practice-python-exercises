e37907da8202766a525b22343dff312d958b2e10
Kevin <mrhallonline@gmail.com>
2019-11-21 21:04:26 -0500

    scraping Jamaica Gleaner instead

--- practicePythonExercises/17decodeAWebpage.py
+++ practicePythonExercises/17decodeAWebpage.py
@@ -1,9 +1,9 @@
 # Use the BeautifulSoup and requests Python packages to print out a list of all the article titles on the New York Times homepage.
 from bs4 import BeautifulSoup
 import requests
-import csv
 
+url = 'http://jamaica-gleaner.com/'
+source = requests.get(url).text
+soup = BeautifulSoup(source, "html5lib")
 
-# url = 'http://github.com'
-# r = requests.get(url)
-# r_html = r.text
+print(soup.prettify())
