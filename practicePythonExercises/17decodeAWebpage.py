861f83e8c50f1ff10a83e314432d7ecf9de008f8
Kevin <mrhallonline@gmail.com>
2019-11-21 20:35:49 -0500

    scraping NYT

--- practicePythonExercises/17decodeAWebpage.py
+++ practicePythonExercises/17decodeAWebpage.py
@@ -1,6 +1,9 @@
 # Use the BeautifulSoup and requests Python packages to print out a list of all the article titles on the New York Times homepage.
-
+from bs4 import BeautifulSoup
 import requests
-url = 'http://github.com'
-r = requests.get(url)
-r_html = r.text
+import csv
+
+
+# url = 'http://github.com'
+# r = requests.get(url)
+# r_html = r.text
