a9413748c1eec3168250c7eeb4185e447ad4ca00
Kevin <mrhallonline@gmail.com>
2019-11-30 17:35:25 -0500

    regex extract email and phone from pdf

--- regularExpressions/extractNumbersAndEmails.py
+++ regularExpressions/extractNumbersAndEmails.py
@@ -1,10 +1,17 @@
-#! python3
+
+import re, pyperclip
+
+
 # TODO: create a regex for phone numbers
-
-
+message = "Rick Jenkins, Associate Director for501-371-206-6501-682-6399URick.Jenkins@adhe.eduUPlanning and AccountabilitySharon Butler501-371-2069501-682-6399USharon.Butler@adhe.eduUProgram SpecialistNO CHILD LEFT BEHIND  PhoneFaxE-  Mail Dr. Suzanne Mitchell501-371-2062501-682-6399USusanne.Mitchell@adhe.eduUNo Child Left Behind Coordinator3"
+phoneRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+print(phoneRegex.findall(message))
 
 
 # TODO create a regex for email addresses
+
+emailRegex = re.compile(r'\w+@\w+.\w\w\w')
+print(emailRegex.findall(message))
 
 
 
