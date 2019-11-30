f501ed430b50725e47028b48c779294999f3e406
Kevin <mrhallonline@gmail.com>
2019-11-30 17:00:49 -0500

    regex extract email and phone from pdf

---
+++ regularExpressions/extractNumbersAndEmails.py
@@ -0,0 +1,21 @@
+#! python3
+# TODO: create a regex for phone numbers
+
+
+
+
+# TODO create a regex for email addresses
+
+
+
+
+# TODO get the text off the clipboard
+
+
+
+
+# TODO extract the email/phone from this text
+
+
+
+# TODO copy the extracted email/phone to the clipboard 
--- regularExpressions/withregularExpressions.py
+++ regularExpressions/withregularExpressions.py
@@ -1,8 +1,9 @@
 import re
+
 # to findall occurrences
-message = "Call me 415-555-1011 tomorrow, or at 415-555-9999 for my office line"
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-print(phoneNumRegex.findall(message))
+# message = "Call me 415-555-1011 tomorrow, or at 415-555-9999 for my office line"
+# phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+# print(phoneNumRegex.findall(message))
 
 # message = "Call me 415-555-1011 tomorrow, or at 415-555-9999 for my office line"
 
@@ -10,6 +11,21 @@ print(phoneNumRegex.findall(message))
 # mo = phoneNumRegex.search(message)
 # print(mo.group())
 
+#making groups in Regex
+
+# message = "My number is 415-555-4242"
+
+# phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+# mo = phoneNumRegex.search(message)
+# print(mo.group(1))
+# print(mo.group(2))
 
 
-# print(mo.group())
+
+# Finding bats
+batRegex= re.compile(r'Bat(man|mobile|copter|bat)')
+mo = batRegex.search('Batmobile lost a wheel')
+print(mo.group())
+
+
+
