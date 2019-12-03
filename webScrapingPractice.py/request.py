a04ef8f586550670a71a0a50642bfd6be16a97c5
Kevin <mrhallonline@gmail.com>
2019-12-03 01:56:43 -0500

    working with Selenium

---
+++ webScrapingPractice.py/request.py
@@ -0,0 +1,12 @@
+import requests
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+res.status_code
+
+print(len(res.text))
+
+
+res.raise_for_status()
+playFile = open('RomeoAndJuliet.txt', 'wb')
+for chunk in res.iter_content(100000):
+    playFile.write(chunk)
+playFile.close()
